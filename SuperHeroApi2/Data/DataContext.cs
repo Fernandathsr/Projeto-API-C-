27f86850c5d3d283706dd910cd599c425dbfd093
Fernandathsr <fernandath.srodrigues@gmail.com>
2024-01-05 17:07:02 -0300

    my first project in c sharp

---
+++ SuperHeroApi2/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApi2.Entities;
+
+namespace SuperHeroApi2.Data
+
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+            
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
---
+++ SuperHeroApi2/Entities/SuperHero.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperHeroApi2.Entities
+
+{
+    public class SuperHero //SuperHero vai herdar as propriedades do "person" 
+    {
+    
+        public int Id { get; set; }
+     
+        public string Name { get; set; }
+
+        public string FirstName { get; set; } = string.Empty; // Quando coloca o string.Empty significa que o campo string será vazio, sem a necessidade de atribuir um valor a ela
+   
+        public string LastName { get; set; } = string.Empty;
+
+        public string Place {  get; set; } = string.Empty;
+    }
+}
